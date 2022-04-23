2a5db13faa585d1fd3109b69c450a21e7f1b4a48
Sedana Yoga <9g.yoga27@gmail.com>
2022-04-23 14:25:13 +0800

    Implemented FoursquareAPI, UnsplashAPI, Geolocation API, SSG, CSR, useContext, useReducer

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,23 +3,29 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import cls from 'classnames'
-
-import coffeeStoresData from '../../data/coffee-stores.json'
+import { fetchCoffeeStores } from '../../lib/coffee-stores'
+// import coffeeStoresData from '../../data/coffee-stores.json'
 import styles from '../../styles/coffee-store.module.css'
+import { useContext, useState, useEffect } from 'react'
+import { StoreContext } from '../../store/store-context'
+import { isEmpty } from '../../utils'
 
-export function getStaticProps(staticProps) {
+export async function getStaticProps(staticProps) {
+  const coffeeStores = await fetchCoffeeStores()
   const params = staticProps.params
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id // dynamic id
+  })
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
-        return coffeeStore.id === +params.id // dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   }
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores()
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
         id: coffeeStore.id.toString(),
@@ -32,14 +38,35 @@ export function getStaticPaths() {
   }
 }
 
-const CoffeeStore = (props) => {
-  const route = useRouter()
-  if (route.isFallback) {
+const CoffeeStore = (initialProps) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
     return <div>Loading...</div>
   }
-  const { address, name, neighbourhood, imgUrl } = props.coffeeStore
+
+  const id = router.query.id
+
+  const [coffeeStore, setCoffeeStore] = useState(initialProps.coffeeStore)
+
+  const {
+    state: { coffeeStores },
+  } = useContext(StoreContext)
+
+  useEffect(() => {
+    if (isEmpty(initialProps.coffeeStore)) {
+      if (coffeeStores.length > 0) {
+        const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+          return coffeeStore.id.toString() === id // dynamic id
+        })
+        setCoffeeStore(findCoffeeStoreById)
+      }
+    }
+  }, [id])
+
+  const { name, address, neighborhood, imgUrl } = coffeeStore
   const handleUpvoteButton = () => {
-    console.log('Handle upvote!')
+    // console.log('Handle upvote!')
   }
   return (
     <div className={styles.layout}>
@@ -58,7 +85,10 @@ const CoffeeStore = (props) => {
           </div>
           <div className={styles.storeImgWrapper}>
             <Image
-              src={imgUrl}
+              src={
+                imgUrl ||
+                'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+              }
               width={600}
               height={360}
               className={styles.storeImg}
@@ -72,10 +102,12 @@ const CoffeeStore = (props) => {
             <Image src='/static/icons/places.svg' width='24' height='24' />
             <p className={styles.text}>{address}</p>
           </div>
-          <div className={styles.iconWrapper}>
-            <Image src='/static/icons/nearMe.svg' width='24' height='24' />
-            <p className={styles.text}>{neighbourhood}</p>
-          </div>
+          {neighborhood && (
+            <div className={styles.iconWrapper}>
+              <Image src='/static/icons/nearMe.svg' width='24' height='24' />
+              <p className={styles.text}>{neighborhood}</p>
+            </div>
+          )}
           <div className={styles.iconWrapper}>
             <Image src='/static/icons/star.svg' width='24' height='24' />
             <p className={styles.text}>1</p>
--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,51 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner'
 import Card from '../components/card'
-// import coffeeStoresData from '../data/coffee-stores.json'
+import { fetchCoffeeStores } from '../lib/coffee-stores'
+import useTrackLocation from '../hooks/use-track-location'
+import { useEffect, useState, useContext } from 'react'
+import { ACTION_TYPES, StoreContext } from '../store/store-context'
 
 export async function getStaticProps(context) {
-  const response = await fetch(
-    'https://api.foursquare.com/v3/places/search?query=coffee%20shop&near=Denpasar&limit=6',
-    {
-      headers: {
-        Authorization: 'fsq3GybytCt+oaqYRRk+DW3uel06sN0dJrmdoPuC+S3aft0=',
-      },
-    }
-  )
-  const data = await response.json()
-  const dataResults = data.results
+  const coffeeStores = await fetchCoffeeStores()
   return {
     props: {
-      coffeeStores: dataResults,
+      coffeeStores,
     },
   }
 }
 
 export default function Home(props) {
+  // const [coffeeStores, setCoffeeStores] = useState([])
+  const { dispatch, state } = useContext(StoreContext)
+  const { coffeeStores, latLong } = state
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null)
+  const { isFindingLocation, handleTrackLocation, locationErrorMsg } =
+    useTrackLocation()
   const handleOnBannerBtnClick = () => {
     console.log('Hi banner button!')
+    handleTrackLocation()
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (latLong) {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30)
+          console.log({ fetchedCoffeeStores })
+          // setCoffeeStores(fetchedCoffeeStores)
+          dispatch({
+            type: ACTION_TYPES.SET_COFFEE_STORES,
+            payload: { coffeeStores: fetchedCoffeeStores },
+          })
+        } catch (err) {
+          console.log({ err })
+          setCoffeeStoresError(err.message)
+        }
+      }
+    }
+    fetchData()
+  }, [latLong])
 
   return (
     <div className={styles.container}>
@@ -37,15 +58,40 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <Banner
-          buttonText='View stores nearby'
+          buttonText={isFindingLocation ? 'Loading...' : 'View stores nearby'}
           handleOnClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg && `Something went wrong: ${locationErrorMsg}`}
+        {coffeeStoresError && `Something went wrong: ${coffeeStoresError}`}
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400} />
         </div>
+
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores Near Me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        )}
+
         {props.coffeeStores.length > 0 && (
-          <>
-            <h2 className={styles.heading2}>Toronto Stores</h2>
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores in Denpasar</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((coffeeStore) => {
                 return (
@@ -62,7 +108,7 @@ export default function Home(props) {
                 )
               })}
             </div>
-          </>
+          </div>
         )}
       </main>
     </div>
